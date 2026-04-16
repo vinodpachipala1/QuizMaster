faa443d2b3978ab99eb286e5575ab34c95a71568
vinodpachipala1 <vinodpachipala93@gmail.com>
2026-04-16 19:35:16 +0530

    url fix

--- frontend/src/config/BaseUrl.jsx
+++ frontend/src/config/BaseUrl.jsx
@@ -1,2 +1,2 @@
-// export const BaseUrl =  "https://quizmaster-l6tq.onrender.com";
-export const BaseUrl =  "http://localhost:3001";
+export const BaseUrl =  "https://quizmaster-l6tq.onrender.com";
+// export const BaseUrl =  "http://localhost:3001";
